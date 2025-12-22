b09a4acb42cb554253f1d5e2d8b0b7813abff6c2
KeyuZhou-rz <2033472649@qq.com>
2025-12-22 23:39:07 +0800

    SDAE_model start

--- SDAE_model.py
+++ SDAE_model.py
@@ -3,8 +3,8 @@ import torch.nn as nn
 
 class SDAE_GCL(nn.module):
     def _init__(self,input_dim=2068,category=11,hidden_dims=[1000,500]):
-        super(SDAE_GCL, self).__init__()
+        super().__init__()
         # conpress 2068 dimenstion features
         self.encoder = nn.Sequential(
-            
+
         )
